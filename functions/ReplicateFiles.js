09cf90176daf924d46caa34fc3a70cf4f01fb62a
Kojji <fernandokojiyama@gmail.com>
2022-12-12 06:15:35 -0300

    add: lambda variables

--- functions/ReplicateFiles.js
+++ functions/ReplicateFiles.js
@@ -1,8 +1,19 @@
-const AWS = require('aws-sdk')
-AWS.config.region = process.env.AWS_REGION 
+const AWS = require('aws-sdk');
+AWS.config.region = process.env.AWS_REGION;
+const Git = require("nodegit");
+const bucketName = process.env.DestinationBucketName;
+const coveredBranch = process.env.CoveredBranch;
+const cloneRepoUrl = process.env.CloneRepoUrl
 
 // The Lambda handler
 exports.handler = async (event) => {
+  
+  console.log(bucketName)
+  console.log(coveredBranch)
+  console.log(cloneRepoUrl)
+  // Git.Clone("https://github.com/nodegit/nodegit", "nodegit").then(function(repository) {
+  //   // Work with the repository object here.
+  // });
   // Params object for SNS
   const params = {
     Message: `Message at ${Date()}`,
